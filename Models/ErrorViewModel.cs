45ec1263e6d0da9b5cf2274967f49e70a5553fcc
ThienLoliKon <23211TT4279@mail.tdc.edu.vn>
2025-10-13 15:32:43 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dating_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
