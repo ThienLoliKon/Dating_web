7c0e80a3631f0af3385c8de4d2087c6bef3ef422
ThienLoliKon <23211TT4279@mail.tdc.edu.vn>
2025-10-21 16:02:44 +0700

    update dll

--- Controllers/CardsController.cs
+++ Controllers/CardsController.cs
@@ -7,4 +7,20 @@ namespace AspnetCoreMvcFull.Controllers;
 public class CardsController : Controller
 {
   public IActionResult Basic() => View();
+  public IActionResult YouLike()
+  {
+    // Tại đây bạn có thể viết code để lấy danh sách những người bạn đã thích
+    // ...
+
+    // Sau đó trả về giao diện của trang YouLike
+    return View();
+  }
+  public IActionResult Chat()
+  {
+    // Tại đây bạn có thể viết code để lấy danh sách những người bạn đã thích
+    // ...
+
+    // Sau đó trả về giao diện của trang YouLike
+    return View();
+  }
 }
